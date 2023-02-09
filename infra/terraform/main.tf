c92049f58d1ffe7a79bbdfb786f5e331c6213a23
zack stevens <zack.st7@gmail.com>
2023-02-08 19:31:45 -0700

    fix: testing things

---
+++ infra/terraform/main.tf
@@ -0,0 +1,9 @@
+module "ui-admin-angular-dev" {
+  source  = "./modules/ui-admin-angular"
+  environment = "dev"
+}
+
+#module "ui-admin-angular-prod" {
+#  source  = "./modules/ui-admin-angular"
+#  environment = "prod"
+#}
