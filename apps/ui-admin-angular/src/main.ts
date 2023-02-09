5b665eedfcab5329d7574abbf15cfc8c5c708a18
zack stevens <zack.st7@gmail.com>
2023-02-09 11:51:00 -0700

    fix: test release

--- apps/ui-admin-angular/src/main.ts
+++ apps/ui-admin-angular/src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from "./environments/environment";
 
+// a change
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   // tslint:disable-next-line:no-console
