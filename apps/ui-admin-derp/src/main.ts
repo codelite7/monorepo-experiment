5e754680105a1b4be420d622c79c35015b1e8345
zack stevens <zack.st7@gmail.com>
2023-02-08 19:38:34 -0700

    fix: some derp fix

--- apps/ui-admin-derp/src/main.ts
+++ apps/ui-admin-derp/src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode, ReflectiveInjector } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from "./environments/environment";
-// stuff
+// stuff more stuff
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   // tslint:disable-next-line:no-console
